1b0068d242d78e1945706f8bcbb86d8b7a845ec2
dev-omega-lambda <dev.omega.lambda@outlook.com>
2024-03-01 16:42:28 -0300

    feat: add controllers for license

--- src/main/java/dev/omegalambda/licensingservice/controller/LicenseController.java
+++ src/main/java/dev/omegalambda/licensingservice/controller/LicenseController.java
@@ -1,9 +1,53 @@
 package dev.omegalambda.licensingservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.omegalambda.licensingservice.model.License;
+import dev.omegalambda.licensingservice.service.LicenseService;
+import lombok.val;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
 public class LicenseController {
+    private final LicenseService licenseService;
+
+    public LicenseController(LicenseService licenseService) {
+        this.licenseService = licenseService;
+    }
+
+    @GetMapping("/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("licenseId") String licenseId,
+            @PathVariable("organizationId") String organizationId
+    ) {
+        val license = licenseService.getLicense(licenseId, organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping()
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License license
+    ) {
+        val responseMsg = licenseService.updateLicense(license, organizationId);
+        return ResponseEntity.ok(responseMsg);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License license
+    ) {
+        val responseMsg = licenseService.createLicense(license, organizationId);
+        return ResponseEntity.ok(responseMsg);
+    }
+
+    @DeleteMapping("/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("licenseId") String licenseId,
+            @PathVariable("organizationId") String organizationId
+    ) {
+        val responseMsg = licenseService.deleteLicense(licenseId, organizationId);
+        return ResponseEntity.ok(responseMsg);
+    }
 }
